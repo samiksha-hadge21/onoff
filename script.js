78e00f19a7e890a624b78cc6fe836eda0a6c8d65
samiksha-hadge21 <samikshahadge21@gmail.com>
2024-12-19 23:55:55 +0530

    Add files via upload
    abc

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function OnNow(){
+    let img = document.getElementById('new')
+    img.src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdDFyc29vY3ZmazRzNGthNndqeW5tM3R0OWpzdGllODZiMXRtNzhlbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6vXTpomeZEyxufGU/giphy.webp"
+}
+function OffNow(){
+    let img = document.getElementById('new')
+    img.src="star.png" 
+}
